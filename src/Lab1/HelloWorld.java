b8bbb3efaa52d800b9d7c9c3a12d5f9cf3072ccc
kanokwan <Kanokwan.we@rmutsvmail.com>
2020-07-15 14:56:14 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,13 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("Hello World !!!");
+        System.out.println("Kanokwan Wetarun");
+        System.out.println("363211760032");
+        System.out.println("เทคโนโลยีการจัดการ");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
+
+
+
 
 
 
