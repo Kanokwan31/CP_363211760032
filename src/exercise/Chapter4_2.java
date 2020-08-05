1203022ee51ffc29032c40527971deb58a9940cc
Kanokwan <kanokwan.we@gmail.com>
2020-08-05 15:50:11 +0700

    Kanokwan

---
+++ src/exercise/Chapter4_2.java
@@ -0,0 +1,25 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class Chapter4_2 {
+    public static void main (String[] args) {
+        //รับช้อมูล จำนวนเต็ม และจำนวนจริง สลับกัน สังเกตผลการทำงาน
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter integer");
+        int a = scanner.nextInt();
+        System.out.print("Enter real number");
+        double  b = scanner.nextDouble();
+        System.out.print("Enter integer");
+        int c = scanner.nextInt();
+        System.out.print("Enter real number");
+        double  d = scanner.nextDouble();
+
+        //display
+        System.out.print(a+" "+b+" "+c+" "+d);
+
+
+    }
+}
