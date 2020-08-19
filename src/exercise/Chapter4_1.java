23bdccf8beafa3a861fafc1660126973cd276593
Kanokwan <kanokwan.we@gmail.com>
2020-08-19 15:47:28 +0700

    Kanokwan

--- src/exercise/Chapter4_1.java
+++ src/exercise/Chapter4_1.java
@@ -1,33 +1,45 @@
-package Exercise;
+package exercise;
 
-public class Chapter4_1{
+import java.util.Scanner;
+
+public class Chapter4_1 {
+
+        //1. รับข้อมูลจำนวนเต็ม 10 จำนวน
+        //2. หาผลรวมรของข้อมูลทั้งหมดแสดงทางหน้าจอภาพ *
+        //3. หาค่าเฉลี่ยของข้อมูลทั้งหมดแสดงทางหน้าจอภาพ *
         public static void main(String[] args) {
-                int a,b,c,d,e,f,g,h,i,j; // ประกาศตัวแปร x สำหรับเก็บข้อมูลจำนวนเต็ม
-                a = 1;
-                b = 2;
-                c = 3;
-                d = 4;
-                e = 5;
-                f = 6;
-                g = 7;
-                h = 8;
-                i = 9;
-                j = 10;
-                System.out.println(a);
-                System.out.println(b);
-                System.out.println(c);
-                System.out.println(d);
-                System.out.println(e);
-                System.out.println(f);
-                System.out.println(g);
-                System.out.println(h);
-                System.out.println(i);
-                System.out.println(j);
-                int x = a+b+c+d+e+f+g+h+i+j;
-                int avg = x / 10;
-                System.out.println("ผลรวม : "+x);
-                System.out.println("ค่าเฉลี่ย : " +avg);
+                Scanner scanner = new Scanner(System.in);
 
+                //input data from keyboard
+                System.out.print("Enter an integer 1: ");
+                int num1 = scanner.nextInt();
+                System.out.print("Enter an integer 2: ");
+                int num2 = scanner.nextInt();
+                System.out.print("Enter an integer 3: ");
+                int num3 = scanner.nextInt();
+                System.out.print("Enter an integer 4: ");
+                int num4 = scanner.nextInt();
+                System.out.print("Enter an integer 5: ");
+                int num5 = scanner.nextInt();
+                System.out.print("Enter an integer 6: ");
+                int num6 = scanner.nextInt();
+                System.out.print("Enter an integer 7: ");
+                int num7 = scanner.nextInt();
+                System.out.print("Enter an integer 8: ");
+                int num8 = scanner.nextInt();
+                System.out.print("Enter an integer 9: ");
+                int num9 = scanner.nextInt();
+                System.out.print("Enter an integer 10: ");
+                int num10 = scanner.nextInt();
+
+                int total = num1 + num2 + num3 + num4 +
+                        num5 + num6 + num7 + num8 + num9 + num10;
+                int avg = total / 10;
+
+                //display result
+                System.out.println("Total : " + total);
+                System.out.println("Average : " + avg);
 
         }
+
 }
---
+++ src/exercise/Exercise_lab3_2.java
@@ -0,0 +1,17 @@
+package exercise;
+
+public class Exercise_lab3_2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=10 ; i++) { // row
+            for (int j = 0; j <=10; j++) { // column
+                System.out.print("*");
+
+            }//j
+            System.out.println();
+        }//i
+
+        
+        
+    }
+}
